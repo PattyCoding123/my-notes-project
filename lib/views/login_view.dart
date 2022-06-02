6d2a801ce516ca4266d2feac7b4c80ae7e63a5e2
Patrick <patrickducusin2@gmail.com>
2022-06-02 02:57:13 -0400

    Fixed grammatical issue in logout dialog and LoginView

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -94,7 +94,7 @@ class _LoginViewState extends State<LoginView> {
                 );
               }
             },
-            child: const Text('Login'),
+            child: const Text('Log in'),
           ),
           TextButton(
             onPressed: () {
