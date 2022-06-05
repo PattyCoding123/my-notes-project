661a6b1502c8fb7fd78e86115f82120fff622e9c
Patrick <patrickducusin2@gmail.com>
2022-06-05 17:58:26 -0400

    step-10 Included flutter test dev dependency into our application. Deleted default widget_test file and made our own test file for testig the auth_provider. Made a mock auth provider to have control over the logic. Created test for each functionality in the auth provder.

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,9 +4,10 @@ import 'package:flutter/foundation.dart';
 @immutable
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
   // Factory constructor that initializes a current user using FirebaseAuth "User"
   // whose isEmailVerified flag is either true or false.
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
